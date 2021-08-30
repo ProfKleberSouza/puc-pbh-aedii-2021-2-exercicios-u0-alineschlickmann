33867795f7d0f0b20437d3e959cbedcefd512a59
alineschlickman <89282564+alineschlickmann@users.noreply.github.com>
2021-08-30 01:39:45 +0000

    exe06

--- exe09/main.c
+++ exe09/main.c
@@ -1,8 +1,64 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
-   return 0;
+//=======================================
+ int main (){
+typedef struct Aluno{
+  char nome[20];
+  int nota;
+}Aluno;
+void print_aluno(Aluno p){
+  printf("{%s, %i,}\n", p.nome, p.nota);
 }
+ //========================================
+ void inserir(Aluno v, int i){
+matriculado[i].nome;
+
+
+ }
+ //========================================
+ int main(void) {
+   Aluno matriculado[];
+   int n;
+
+
+  
+  
+  
+  
+  
+  
+   return 0;
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ }
